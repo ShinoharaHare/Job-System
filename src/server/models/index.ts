eb22fe4943cccdc7d23cefd6cb2e3c043dc67ade
Hare <anna.ahiker1822@gmail.com>
2020-12-07 02:23:41 +0800

    style: 簡化export語法
    調整：
    modes\index.ts

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -10,7 +10,7 @@ connect(config.MONGODB_URI, {
     useFindAndModify: false
 })
 
-export { Account, IAccount, DAccount } from './account'
-export { Applyment, IApplyment, DApplyment } from './applyment'
-export { Job, IJob, DJob } from './job'
-export { Tag, ITag, DTag } from './tag'
+export * from './account'
+export * from './applyment'
+export * from './job'
+export * from './tag'
