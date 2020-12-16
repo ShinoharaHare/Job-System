34f0c0357d4bcf8b9fa4a32ed591e45fc05eb1d0
Hank Lin <menghan1117@gmail.com>
2020-12-16 21:53:59 +0800

    fix: 修正畫面重整後帳戶資料會消失的情況

--- src/client/App.vue
+++ src/client/App.vue
@@ -14,9 +14,16 @@ import { Vue, Component } from 'vue-property-decorator'
 import SystemMessage from '@/client/components/SystemMessage.vue'
 import Navigation from '@/client/components/Navigation.vue'
 
+import { namespace } from 'vuex-class'
+const Account = namespace('Account')
+
 @Component({ components: { SystemMessage, Navigation } })
 export default class extends Vue {
+    @Account.Action getAccountInfo!: Function
 
+    created() {
+        this.getAccountInfo()
+    }
 }
 </script>
 
