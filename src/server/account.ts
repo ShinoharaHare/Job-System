5a8a990071af502bce487e4e04bebd3c41e33662
00757001 <00757001@email.ntou.edu.tw>
2020-12-24 01:42:20 +0800

    feat:修改忘記密碼和重設密碼

--- src/server/account.ts
+++ src/server/account.ts
@@ -1,63 +1,78 @@
-import { Types } from 'mongoose'
-import { Account } from '@/server/models'
+import { Types, Document } from 'mongoose'
+import { Account, DAccount } from '@/server/models'
 import * as nodemailer from 'nodemailer'
 import jwt from 'jsonwebtoken'
 import config from '@/server/config'
 
+import { SubDocumentArray, SubDocument, SubDocumentArrayNoId, SubDocumentNoId } from 'ts-mongoose/types/_shared'
+
+function makeid(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 // 忘記密碼
-export const findPwd = (email: string) => {
-    const account = Account.findOne({
-        email: email
-    })
+export const findPwd = async(email: string) => {
+    // const email = "00757001@email.ntou.edu.tw"
+    const user = await Account.findOne({ email: email })
+    const validCode = makeid(6)
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hungjiewu@gmail.com',
+            pass: 'vwlspvrrtewwzcga'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    const mailOptions = {
+        from: '"小雞雞上工" <hungjiewu@gmail.com>',
+        to: email,
+        subject: '帳號重設連結',
+        html: `<h2>請使用以下驗證碼來重設密碼</h2>
+                <h4 style="color:red;">請在10分鐘內完成驗證動作</h4>
+                <h3 style="color:blue;">${validCode}</h3>`
+    };
+    transporter.sendMail(mailOptions, (error: any, info: any) => {
+        if (error) {
+            console.log(`error: ${error}`);
+        } else {
+            console.log(`Message Sent ${info.response}`);
+        }
+    });
+    user!.resetPwd!.code = validCode
+    user!.resetPwd!.expired = new Date()
+}
 
-    if (account === null) {
-        return 404;
+export const checkValidation = async(email: string, code: string) => {
+    const user = await Account.findOne({ email: email })
+    const nowTime = new Date()
+    console.log(user!.resetPwd!.expired)
+    console.log(user!.resetPwd!.code)
+    if (user!.resetPwd!.expired!.getTime() > Date.now() - 10 * 60 * 1000) { // 10*60*1000 = 十分鐘
+        if (user!.resetPwd!.code === code) {
+            console.log('驗證碼正確')
+            return true
+        } else {
+            console.log('驗證碼錯誤')
+            return false// '驗證碼錯誤'
+        }
     } else {
-        const transporter = nodemailer.createTransport(
-            'smtps://<username>%40gmail.com:<password>@smtp.gmail.com'
-        );
-        // const token = jwt.sign({ id: account._id }, config.RESET_PASSWORD, { expiresIn: '20m' })
-        const mailOptions = {
-            from: '00757001@email.ntou.edu.tw',
-            to: email,
-            subject: '帳號重設連結',
-            html: `
-                <h2>請點擊以下連結來重設密碼</h2>
-                <p>${process.env.CLIENT_URL}/resetpassword/</p>
-            `
-        };
-        transporter.sendMail(mailOptions, (error: any, info: any) => {
-            if (error) {
-                return `error: ${error}`;
-            } else {
-                return `Message Sent ${info.response}`;
-            }
-        });
+        return false// '驗證碼已過期'
     }
 }
-
-// export const comment
 // 重設密碼
-export const resetPassword = (user: Types.ObjectId, hash: string) => {
-    try {
-        Account.update({ _id: user }, { $set: { hash: hash } })
-        return 200
-        // res.status(200).json()
-    } catch (error) {
-        return `error: ${error}`;
-    }
-}
-// 新增收藏工作
-export const addFavorite = (user: Types.ObjectId, job: Types.ObjectId) => {
+export const resetPassword = async(email: string, newHash: string) => {
     try {
         Account.update(
-            { _id: user },
-            {
-                $push: {
-                    favorite: job
-                }
-            })
-        return 200
+            { email: email },
+            { $set: { hash: newHash } })
     } catch (error) {
         return `error: ${error}`;
     }
@@ -153,3 +168,6 @@ export const updateResume = (user: Types.ObjectId, resume: Types.ObjectId, name:
         return `error: ${error}`;
     }
 }
+
+// findPwd("hungjiewu@gmail.com")
+// checkValidation("hungjiewu@gmail.com","dKHOWB")
