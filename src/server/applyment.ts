e8e737d0dca69071de8624ab5d8d5b66b9645fbb
Kenneth Ding <58462256+Kenneth-Ding@users.noreply.github.com>
2020-12-23 12:28:34 +0800

    applyment_test

--- src/server/applyment.ts
+++ src/server/applyment.ts
@@ -1,10 +1,68 @@
-import { DApplyment } from './models';
+import { DApplyment, Applyment } from './models';
 
-function bossAccept(applyment: DApplyment) {
+import { Types } from 'mongoose'
+
+import { sendNots } from '@/server/notification'
+
+const bossAccept = (applyment: DApplyment) => {
     applyment.state = 1
     applyment.save()
 }
-function bossRefuse(applyment: DApplyment) {
+
+const applicantAccept = (applyment: DApplyment) => {
+    applyment.state = 2
+    applyment.save()
+}
+
+const applicantRefuse = (applyment: DApplyment) => {
+    applyment.state = 3
+    applyment.save()
+}
+
+const bossRefuse = (applyment: DApplyment) => {
     applyment.state = 4
     applyment.save()
+}
+
+const applicantGiveup = (applyment: DApplyment) => {
+    applyment.state = 5
+    applyment.save()
+}
+
+export const createApplyment = async(applicant: Types.ObjectId, job: Types.ObjectId, resume: string) => {
+    await Applyment.create({
+        applicant: applicant,
+        job: job,
+        resume: resume
+    });
+    console.log('Applyment成功創建')
+}
+export const bossAcceptApplyment = async(applicant: Types.ObjectId) => {
+    const target = await Applyment.findById(applicant)
+    if (target) {
+        bossAccept(target)
+        sendNots(applicant, '')
+    }
+    console.log('老闆成功接受')
+}
+export const bossRefuseApplyment = async(applicant: Types.ObjectId) => {
+    const target = await Applyment.findById(applicant)
+    if (target) { bossRefuse(target) }
+    console.log('老闆成功拒絕')
+}
+export const applicantAcceptApplyment = async(applicant: Types.ObjectId) => {
+    const target = await Applyment.findById(applicant)
+    if (target) { applicantAccept(target) }
+    console.log('申請人成功拒絕')
+}
+export const applicantGiveupApplyment = async(applicant: Types.ObjectId) => {
+    const target = await Applyment.findById(applicant)
+    if (target) { applicantGiveup(target) }
+    console.log('申請人成功放棄')
+}
+
+export const applicantRefuseApplyment = async(applicant: Types.ObjectId) => {
+    const target = await Applyment.findById(applicant)
+    if (target) { applicantRefuse(target) }
+    console.log('申請人成功拒絕')
 }
--- src/server/routes/api/applyment.ts
+++ src/server/routes/api/applyment.ts
@@ -1,10 +1,19 @@
 import { auth, required } from '@/server/middlewares'
 import { Router } from 'express'
+import { bossRefuseApplyment } from '@/server/applyment'
+import { Types } from 'mongoose'
 
 const router = Router()
 
 router.post('/', auth, required('job', 'resume'), async(req, res) => {
 
+})
+
+router.get('/test', (req, res) => {
+    bossRefuseApplyment(Types.ObjectId('5fde0206359f4c512ce389be'))
+    res.json(
+        'refuse success'
+    )
 })
 
 router.get('/', auth, async(req, res) => {
