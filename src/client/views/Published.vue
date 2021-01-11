d291a8feffec438941171d47dea6bc3549a81f1f
Binray Shen <binray.shen@gmail.com>
2021-01-12 04:37:35 +0800

    fix: 修復顯示已刊登工作

--- src/client/views/Published.vue
+++ src/client/views/Published.vue
@@ -3,15 +3,36 @@ v-card(tile, height="100%")
     v-toolbar(dark, color="primary")
         v-toolbar-title 刊登管理
 
-    v-expansion-panels.mt-8(tile, popout, focusable)
-        v-expansion-panel(v-for="({ name }, i) in jobs", :key="i")
-            v-expansion-panel-header {{ name }}
-            v-expansion-panel-content
-                v-card-actions
-                    v-spacer
-                    v-btn(color="error") 移除
-                    v-btn(color="success") 修改
-                    v-btn(color="warning", @click="showCandidates = true") 應徵者
+    v-list(two-line, outlined)
+        v-list-group(v-for="({ title, content }, i) in jobs", :key="i")
+            template(v-slot:activator)
+                v-list-item-content.mx-5
+                    v-list-item-title
+                        h3 {{ title }}
+                    v-list-item-subtitle
+                        v-chip.mr-1(
+                            x-small,
+                            color="primary",
+                            :key="i",
+                            v-for="(tag, i) in jobs.tags"
+                        ) {{ tag }}
+                
+            v-divider
+
+            v-list-item
+                v-list-item-content
+                    v-list-item-title {{ content }}
+            
+            v-list-item
+                v-list-item-content(align="right ")
+                    span
+                        v-btn.mr-1(color="error") 移除
+                        v-btn.mr-1(color="success") 修改
+                        v-btn.mr-1(color="warning", @click="showCandidates = true") 應徵者
+
+            v-divider
+
+            
 
     v-btn(fixed, bottom, right, fab, dark, color="primary", to="/job/new")
         v-icon mdi-plus
@@ -22,18 +43,23 @@ v-card(tile, height="100%")
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { namespace } from 'vuex-class'
+import { IAccount } from '@/server/models'
 import CandidatesDialog from '@/client/components/CandidatesDialog.vue'
 import EditJobDialog from '@/client/components/EditJobDialog.vue'
 
+const Account = namespace('Account')
+
 @Component({ components: { CandidatesDialog, EditJobDialog } })
 export default class extends Vue {
+    @Account.State account!: IAccount
     jobs: any[] = []
     showCandidates = false
     showEditor = false
 
     async mounted() {
         // console.log(69, await axios.get('api/job'));
-        const { data } = await axios.get('api/job');
+        const { data } = await axios.get('api/job', { params: this.account._id });
         this.jobs = data;
     }
 }
